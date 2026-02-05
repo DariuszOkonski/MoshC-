dc6ab5f44a93e09ee6c801c0e7f640a993b7cdd3
Dariusz Okoński <darek200180@gmail.com>
2026-02-05 08:20:16 +0100

    initialization

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SandboxNET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            checked
+            {
+                byte number = 250;
+                number += 3;
+
+                Console.WriteLine(number);
+            }
+
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SandboxNET")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SandboxNET")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("131b17c3-d9f5-4b5b-a70f-ff58745fa67a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
