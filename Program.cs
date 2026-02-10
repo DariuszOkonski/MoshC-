9743b768736df03a8188c79d901bcd78611488c4
Dariusz Okoński <darek200180@gmail.com>
2026-02-10 08:37:22 +0100

    start with classes

--- Program.cs
+++ Program.cs
@@ -1,23 +1,9 @@
-﻿using System;
-
-namespace SandboxNET
+﻿namespace SandboxNET
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            try
-            {
-                var number = "1234";
-                byte b = Convert.ToByte(number);
-                Console.WriteLine(b);
-
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Something went wrong");
-            }
-
 
         }
     }
