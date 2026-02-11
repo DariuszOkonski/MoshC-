bc87abba23bdfceb2c8f2215347375a0abd6116e
Dariusz Okoński <darek200180@gmail.com>
2026-02-11 07:54:28 +0100

    add class Person

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace SandboxNET
+﻿using System;
+
+namespace SandboxNET
 {
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is " + FirstName + " " + LastName);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Person john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Dear";
 
+            john.Introduce();
         }
     }
 }
