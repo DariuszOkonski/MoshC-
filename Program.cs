d41cc3ccc242bcefc16b286c4b511ebed8d3899e
Dariusz Okoński <darek200180@gmail.com>
2026-02-11 08:13:45 +0100

    present arrays

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using SandboxNET.Math;
-using System;
+﻿using System;
 
 namespace SandboxNET
 {
@@ -7,16 +6,24 @@ namespace SandboxNET
     {
         static void Main(string[] args)
         {
-            Person john = new Person();
-            john.FirstName = "John";
-            john.LastName = "Smith";
+            var numbers = new int[3];
+            numbers[0] = 1;
 
-            john.Introduce();
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
 
-            Calculator calculator = new Calculator();
+            var flags = new bool[3];
+            flags[0] = true;
 
-            var result = calculator.Add(1, 2);
-            Console.WriteLine(result);
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] { "Jack", "John", "Mary" };
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
         }
     }
 }
