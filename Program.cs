e31343c8af4d27938363e3c9326dde1498f5f095
Dariusz Okoński <darek200180@gmail.com>
2026-02-13 07:52:21 +0100

    move ShippingMethod to separate file

--- Program.cs
+++ Program.cs
@@ -1,29 +1,12 @@
-﻿using System;
-
-namespace SandboxNET
+﻿namespace SandboxNET
 {
-    public enum ShippingMethod
-    {
-        RegularAirMail = 1,
-        RegisteredAirMail,
-        Express
-    }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            int hour = 10;
-
-            if (hour > 0 && hour < 12)
-            {
-                Console.WriteLine("It's morning");
-            }
-            else
-            {
-                Console.WriteLine("It's evening");
-            }
-
+            bool isGoldCustomer = true;
+            float price = isGoldCustomer ? 19.95f : 29.95f;
 
 
 
---
+++ ShippingMethod.cs
@@ -0,0 +1,9 @@
+﻿namespace SandboxNET
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail,
+        Express
+    }
+}
