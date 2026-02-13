dc13ba7c0407e6c1407c0ff9eae3c10efbbe1eaa
Dariusz Okoński <darek200180@gmail.com>
2026-02-13 08:32:26 +0100

    finish exercise 1

--- Exercises/Exercise_5_1.cs
+++ Exercises/Exercise_5_1.cs
@@ -1,12 +1,98 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SandboxNET.Exercises
 {
     internal class Exercise_5_1
     {
+        public static void RunProgramOne()
+        {
+            Console.WriteLine("Enter a number between 1-10: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number >= 1 && number <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+
+        public static void RunProgramTwo()
+        {
+            Console.WriteLine("Provide two numbers");
+            Console.Write("Number 1: ");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.Write("Number 2: ");
+            int number2 = int.Parse(Console.ReadLine());
+
+            if (number1 > number2)
+            {
+                Console.WriteLine("Max: " + number1);
+            }
+            else
+            {
+                Console.WriteLine("Max: " + number2);
+            }
+        }
+
+        public static void RunProgramThree()
+        {
+            Console.WriteLine("Provide width and height");
+            Console.Write("Width: ");
+            int width = int.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            int height = int.Parse(Console.ReadLine());
+
+            if (width > height)
+            {
+                Console.WriteLine("Landscape");
+            }
+            else if (width < height)
+            {
+                Console.WriteLine("Portrait");
+            }
+            else
+            {
+                Console.WriteLine("Square");
+            }
+        }
+
+        public static void RunProgramFour()
+        {
+            Console.WriteLine("Speed Camera");
+            Console.Write("What is speed limit: ");
+            int speedLimit = int.Parse(Console.ReadLine());
+            Console.Write("What is current speed: ");
+            int currentSpeed = int.Parse(Console.ReadLine());
+
+            const int OVERSPEED_POINTS = 5;
+
+            if (currentSpeed < speedLimit + OVERSPEED_POINTS)
+            {
+                Console.WriteLine("OK");
+                return;
+            }
+
+
+            int overSpeedKilometersPerHour = currentSpeed - speedLimit;
+            int demeritPoints = overSpeedKilometersPerHour / OVERSPEED_POINTS;
+
+            if (demeritPoints <= 12)
+            {
+                Console.WriteLine(string.Format("Overspeed, you have {0} demerit points", demeritPoints));
+            }
+            else
+            {
+                Console.WriteLine("Licence suspended");
+
+            }
+
+        }
+        public static void Run()
+        {
+            RunProgramFour();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace SandboxNET
+﻿using SandboxNET.Exercises;
+
+namespace SandboxNET
 {
 
     internal class Program
@@ -8,8 +10,26 @@
             //bool isGoldCustomer = true;
             //float price = isGoldCustomer ? 19.95f : 29.95f;
 
+            //var season = Season.Autumn;
 
+            //switch (season)
+            //{
+            //    case Season.Spring:
+            //        break;
+            //    case Season.Summer:
+            //        Console.WriteLine("It's perfect to go to beach");
+            //        break;
+            //    case Season.Autumn:
+            //        Console.WriteLine("It's autumn and a beautiful season.");
+            //        break;
+            //    case Season.Winter:
+            //        break;
+            //    default:
+            //        Console.WriteLine("I don't understand that season");
+            //        break;
+            //}
 
+            Exercise_5_1.Run();
 
         }
     }
