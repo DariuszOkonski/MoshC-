c32139a7e2cf173675df12857d2635faaa12fcef
Dariusz Okoński <darek200180@gmail.com>
2026-03-03 08:32:24 +0100

    working on loops

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using SandboxNET.Exercises;
+﻿using System;
 
 namespace SandboxNET
 {
@@ -7,30 +7,14 @@ namespace SandboxNET
     {
         static void Main(string[] args)
         {
-            //bool isGoldCustomer = true;
-            //float price = isGoldCustomer ? 19.95f : 29.95f;
+            int i = 0;
 
-            //var season = Season.Autumn;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
 
-            //switch (season)
-            //{
-            //    case Season.Spring:
-            //        break;
-            //    case Season.Summer:
-            //        Console.WriteLine("It's perfect to go to beach");
-            //        break;
-            //    case Season.Autumn:
-            //        Console.WriteLine("It's autumn and a beautiful season.");
-            //        break;
-            //    case Season.Winter:
-            //        break;
-            //    default:
-            //        Console.WriteLine("I don't understand that season");
-            //        break;
-            //}
-
-            Exercise_5_1.Run();
-
+                i++;
+            }
         }
     }
 }
